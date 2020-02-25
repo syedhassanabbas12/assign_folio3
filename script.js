e668473a809596b0c27948e3742ca1812e7b6d32
Syed Hassan Abbas <hassanabbassayed@gmail.com>
2020-02-25 04:40:57 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,117 @@
-var el = document.getElementById('list');
+const listSelector = document.querySelectorAll('#list li'),
+    tabs = [];
+for (let i = 0; i < listSelector.length; i++) {
+    tabs.push(listSelector[i].innerHTML);
+    console.log(tabs.indexOf(""));
+    alert(listSelector[i].textContent);
+}
 
 function addItem() {
+    // console.log("addItem()");
     var list = document.getElementById("list");
+    // var list2 = document.getElementById("list2");
     var inputText = document.getElementById("inputField").value
+    
+    if (!inputText) { // If input is empty
+        alert("Can not add empty To-Do(s)");
+        return;
+    } else { // if input is already in the list
+        var listItems = document.getElementById("list").childNodes;
+        for (var i = 0; i < listItems.length; i++) {
+            if (inputText == listItems[i].childNodes[1].data) {
+                alert("To-do Already Exist!");
+                return;
+            }
+        }
+    }
+    //creating new todo
     var tag = document.createElement("li");
     tag.setAttribute('draggable', 'true');
     tag.setAttribute('id', Math.random());
     tag.className = "list-group-item column";
-    // tag.classList = "list-group-item column"
-    tag.innerHTML = inputText + ' <button id="btn" onclick="removeItem(this)">x</button>';
+    tag.innerHTML = '<input type="checkbox" name="category" value="One" onclick="selectTodo(this)">' + inputText +
+    '<button id="btn" onclick="removeItem(this)">x</button>' +
+    '<button id="btnEdit" onclick="editMe(this)">Edit</button>' +
+    '<label class="switch" onclick="statusChange(this)"><input type="checkbox"><span class="slider round"></span></label>' +
+    '<p id="status">pending</p>';
     list.appendChild(tag);
-
+    document.getElementById("inputField").value = "";
     var cols = document.querySelectorAll('#list .column');
     [].forEach.call(cols, addDnDHandlers);
 }
 
-
-function removeItem(t) {
-    console.log(t.parentNode)
-    document.getElementById("list").removeChild(t.parentNode);
+function editMe(me) {
+    // console.log("editMe()");
+    var todo = prompt("Edit this To-Do:", me.parentNode.childNodes[1].data);
+    var oldCheck = me.childNodes[0].checked;
+    if (todo == null || todo == "") {
+        alert("Can not add empty To-Do(s)");
+        return;
+    } else {
+        var list = document.getElementById("list").childNodes;
+        for (var i = 0; i < list.length; i++) {
+            if (todo == list[i].childNodes[1].data) {
+                alert("To-do Already Exist!");
+                return;
+            }
+        }
+        // list[0].childNodes[1].data = todo;
+        txt = '<input type="checkbox" name="category" value="One" onclick="selectTodo(this)">' + todo +
+            '<button id="btn" onclick="removeItem(this)">x</button>' + '<button id="btnEdit" onclick="editMe(this)">Edit</button>' +
+            '<label class="switch" onclick="statusChange(this)"><input type="checkbox"><span class="slider round"></span></label>' +
+            '<p id="status">' + me.parentNode.childNodes[5].innerHTML + '</p>';
+    }
+    me.parentNode.innerHTML = txt;
 }
 
+function removeItem(me) {
+    // console.log("removeItem()");
+    if (tabs.length) {
+        for (var i = tabs.length - 1; i >= 0; i--) {
+            document.getElementById("list").removeChild(tabs[i]);
+            tabs.pop();
+        }
+    } else {
+        document.getElementById("list").removeChild(me.parentNode);
+    }
+}
 
+function selectTodo(me) {
+    // console.log('selectTodo');
+    if (me.checked) {
+        tabs.push(me.parentNode)
+    } else {
+        tabs.splice(tabs.indexOf(me.parentNode), 1);
+    }
+}
 
+function statusChange(me) {
+    // console.log('statusChange()', me.parentNode.childNodes[5].innerHTML);
+    var status = me.childNodes[0].checked;
+    if (status) {
+        me.parentNode.classList.toggle('columnPending');
+        me.parentNode.childNodes[5].innerHTML = "Done";
+        console.log(me.parentNode.childNodes);
 
+        // var tag = document.createElement("li");
+        // tag.setAttribute('draggable', 'true');
+        // tag.setAttribute('id', Math.random());
+        // tag.className = "list-group-item column";
+        // tag.innerHTML = '<input type="checkbox" name="category" value="One" onclick="selectTodoDone(this)">' +
+        //     'me.parentNode.childNodes[1].data' +
+        //     '<button id="btn" onclick="removeItemDone(this)">x</button>' +
+        //     '<button id="btnEdit" onclick="editMeDone(this)">Edit</button>' +
+        //     '<label class="switch" onclick="statusChange(this)"><input type="checkbox"><span class="slider round"></span></label>' +
+        //     '<p id="status">Done</p>';
 
-
-
-
+        document.getElementById("list2").appendChild(me.parentNode);
+        var cols = document.querySelectorAll('#list2 .column2');
+        [].forEach.call(cols, addDnDHandlers);
+        removeItem(me);
+    } else {
+        me.parentNode.childNodes[5].innerHTML = "Pending";
+    }
+}
 
 
 
@@ -51,13 +135,14 @@ function handleDragStart(e) {
 
     this.classList.add('dragElem');
 }
+
 function handleDragOver(e) {
     if (e.preventDefault) {
         e.preventDefault(); // Necessary. Allows us to drop.
     }
     this.classList.add('over');
 
-    e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+    e.dataTransfer.dropEffect = 'move'; // See the section on the DataTransfer object.
 
     return false;
 }
@@ -67,7 +152,7 @@ function handleDragEnter(e) {
 }
 
 function handleDragLeave(e) {
-    this.classList.remove('over');  // this / e.target is previous target element.
+    this.classList.remove('over'); // this / e.target is previous target element.
 }
 
 function handleDrop(e) {
